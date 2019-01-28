2309756ee6b5a8cf0fb715cd1114a433fda215dd
Inchul <yoan.avisse@gmail.com>
2019-01-28 14:43:01 +0100

    Corrections in class Game

--- Game/Game/Game.swift
+++ Game/Game/Game.swift
@@ -1,7 +1,10 @@
 class Game {
-    func welcomeMessage() {
+    func start() {
         print("Welcome to this little fighting game where you can oppose two teams of three characters !")
         // A compléter
+        team1.namePlayer1()
+        team1.createTeam()
+        team2.namePlayer2()
+        team2.createTeam()
     }
-    
 }
--- Game/Game/main.swift
+++ Game/Game/main.swift
@@ -1,9 +1,6 @@
 // P3 Game
 
 let game = Game()
-game.welcomeMessage()
-team1.namePlayer1()
-team1.createTeam()
-team2.namePlayer2()
-team2.createTeam()
+game.start()
+
 
